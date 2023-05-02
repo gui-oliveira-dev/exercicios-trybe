99ea6b576e34b90c1b4330095d288cde7a6adf49
Guilherme Oliveira <70778992+gui-oliveira-dev@users.noreply.github.com>
2023-05-02 16:34:19 -0300

    cria secao 3

---
+++ Fundamentos/3. Introdução a java script/Dia 01: JavaScript - Primeiros passos/if-else.js
@@ -0,0 +1,14 @@
+person = "joana"
+note = 24
+
+if (note >= 80) {
+  console.log("Parabéns, você foi aprovada");
+}
+
+else if (note >= 60 && note < 80) {
+  console.log("Você está na nossa lista de espera");
+}
+
+else if (note < 60) {
+  console.log("Você foi reprovada");
+}
