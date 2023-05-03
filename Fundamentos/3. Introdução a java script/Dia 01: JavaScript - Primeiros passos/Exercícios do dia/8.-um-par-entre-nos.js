5b5f5d43443dda7c6f7f98629486aadaa49f5aba
Guilherme Oliveira <70778992+gui-oliveira-dev@users.noreply.github.com>
2023-05-03 15:32:21 -0300

    exercicios 1 ao 10 prontos

---
+++ Fundamentos/3. Introdução a java script/Dia 01: JavaScript - Primeiros passos/Exercícios do dia/8.-um-par-entre-nos.js
@@ -0,0 +1,13 @@
+/* Utilize if/else para escrever um código que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, o código deve retornar false.
+
+Bônus: use somente um if. */
+
+let a = 21;
+let b = 25;
+let c = 33;
+
+if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+  console.log("true");
+} else {
+  console.log("false");
+}
---
+++ Fundamentos/3. Introdução a java script/Dia 01: JavaScript - Primeiros passos/Exercícios do dia/9.um-impar-entre-nos.js
@@ -0,0 +1,11 @@
+/* Utilize if/else para escrever um código que defina três números em variáveis e retorne true se pelo menos uma das três for ímpar. Caso contrário, o código deve retornar false. */
+
+let a = 20;
+let b = 26;
+let c = 35;
+
+if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
