2b14fe342a5d5ad1c6bba10ceb8ed43900809789
Guilherme Oliveira <70778992+gui-oliveira-dev@users.noreply.github.com>
2023-05-03 20:41:29 -0300

    finaliza exercicio lidando com arrays

---
+++ Fundamentos/3. Introdução a java script/Dia 02: JavaScript - Array e loop For/para-fixar/arrays(listas).js
@@ -0,0 +1,25 @@
+
+
+//Obtenha o valor “Serviços” do array menu:
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1] ;
+[1]
+console.log(menuServices);
+
+
+// Procure o índice do valor “Portfólio” do array menu:
+
+let indexOfPortfolio = menu[2] ;
+
+console.log(indexOfPortfolio);
+
+
+// Adicione o valor “Contato” no final do array menu.
+
+menu.push('Contato')
+let menuContacts = menu[menu.length - 1]
+
+console.log(menuContacts);
+
+
