a67476ee0f852770fbb9792794b1cb3ffe673baa
Guilherme <guigo@pop-os.localdomain>
2023-05-16 18:41:39 -0300

    inicio das atividades

---
+++ Fundamentos/4. Java script: DOM, eventos e web storage/Dia 01: JavaScript - DOM e seletores/seletores e elementos/mudancas.js
@@ -0,0 +1,4 @@
+document.getElementById("page-title").innerText = "Os incríveis";
+ const paraph2 = document.getElementById("second-paragraph")
+  paraph2.style.color = 'green'
+  paraph2.innerText = 'O flecha corre muito rápido'
---
+++ Fundamentos/4. Java script: DOM, eventos e web storage/Dia 02: JavaScript - Trabalhando com elementos/Buscando elementos/script.js
@@ -0,0 +1,11 @@
+// Exer 1
+
+const exer1 = document.getElementById('where-are-you')
+console.log(exer1);
+
+// Exer 2
+
+ const exer2 = exer1.parentNode
+const mudacor = exer2
+
+console.log(exer2);
